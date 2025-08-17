13d274af9e877c39a39f163e8c638f21473badad
chronoDave <davidwolters6758@gmail.com>
2025-08-17 16:55:46 +0100

    Added list no-op

--- src/lib/dom/list.ts
+++ src/lib/dom/list.ts
@@ -11,8 +11,13 @@ export default <T>(component: Component<T>) =>
       while (root.children.length > next.length) root.lastChild?.remove();
 
       next.forEach((data, i) => {
-        /** Create and cache element */
+        const child = root.children.item(i);
         let element = cache.get(data);
+
+        /** If data is cached and unchanged, return */
+        if (element === child) return; 
+
+        /** Create and cache element */
         if (!element) {
           element = component(data);
           cache.set(data, element);
@@ -25,11 +30,7 @@ export default <T>(component: Component<T>) =>
           refs.add(element);
         }
 
-        /**
-         * If child exists at current index replace with element,
-         * otherwise append element
-         */
-        const child = root.children.item(i);
+        /** If child exists at current index replace with element, otherwise append element */
         if (child) {
           root.replaceChild(element, child);
         } else {
