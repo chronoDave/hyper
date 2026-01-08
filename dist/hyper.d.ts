199d3e3da068841b19d714e3dccc906cd7204be1
chronoDave <davidwolters6758@gmail.com>
2026-01-08 15:51:54 +0000

    0.5.0

--- dist/hyper.d.ts
+++ dist/hyper.d.ts
@@ -15,7 +15,7 @@ declare const _default: <T extends keyof HTMLElementTagNameMap>(tag: T) => <P ex
 declare const svg: <T extends keyof SVGElementTagNameMap>(tag: T) => <P extends Attributes>(attributes?: P | undefined) => (...children: Child[]) => SVGElementTagNameMap[T];
 declare const mathml: <T extends keyof MathMLElementEventMap>(tag: T) => <P extends Attributes>(attributes?: P | undefined) => (...children: Child[]) => MathMLElement;
 declare const xml: (tag: string) => <P extends Attributes>(attributes?: P) => (...children: Child[]) => HTMLElement;
-declare const list: <T>(render: (data: T, i: number) => Element) => (root: Element) => (next: T[]) => void;
+declare const list: <T extends string | number>(render: (data: T, i: number) => Element) => (root: Element) => (next: T[]) => void;
 
 export { _default as default, env, list, mathml, svg, xml };
 export type { Attributes, Child, HTMLVoidElementTagName };
